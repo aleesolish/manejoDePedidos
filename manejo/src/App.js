f737b8dde20113aaaa9a51af42ab772087cefecd
Carlos Daniel Cuellar <34011338+fleadan@users.noreply.github.com>
2020-10-23 21:53:20 -0500

    daewawd

--- manejo/src/App.js
+++ manejo/src/App.js
@@ -1,51 +1,45 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-import Dragula from "react-dragula";
+import dragula from "react-dragula";
 import DragulaStyles from "react-dragula/dist/dragula.min.css";
 
 export default function App() {
-  let left = React.createRef();
-  let right = React.createRef();
+  let sal = React.createRef();
+  let lcd = React.createRef();
+  let proc = React.createRef();
+  let succ = React.createRef();
+  let fail = React.createRef();
 
   useEffect(() => {
-    let lElement = ReactDOM.findDOMNode(left.current);
-    let rElement = ReactDOM.findDOMNode(right.current);
-    Dragula([lElement, rElement], {});
+    let salElement = ReactDOM.findDOMNode(sal.current);
+    let lcdElement = ReactDOM.findDOMNode(lcd.current);
+    let procElement = ReactDOM.findDOMNode(proc.current);
+    let succElement = ReactDOM.findDOMNode(succ.current);
+    let failElement = ReactDOM.findDOMNode(fail.current);
+    var drake = dragula([salElement, lcdElement, procElement, succElement, failElement], {removeOnSpill: true});
   }, []);
 
   return (
     <div className="App">
       <h1>Manejador de pedidos</h1>
+      <button type="submit">Crear Pedido</button>
       <div className="Container">
-        <div class="salida"ref={left}>
-          <div>Swap me around</div>
-          <div>Swap her around</div>
-          <div>Swap him around</div>
-          <div>Swap them around</div>
+      
+        <div class="salida"ref={sal}> Salida De Planta
+          
         </div>
-        <div class="local"ref={right}>
-          <div>Swap us around</div>
-          <div>Swap things around</div>
-          <div>Swap everything around</div>
+        <div class="local"ref={lcd}>Local Delivery Center
+          
         </div>
-        <div  class="proceso"ref={left}>
-          <div>Swap me around</div>
-          <div>Swap her around</div>
-          <div>Swap him around</div>
-          <div>Swap them around</div>
+        <div  class="proceso"ref={proc}>En proceso de Entrega
+          
         </div>
-        <div class="exito" ref={left}>
-          <div>Swap me around</div>
-          <div>Swap her around</div>
-          <div>Swap him around</div>
-          <div>Swap them around</div>
+        <div class="exito" ref={succ}> Entrega Completa
+          
         </div>
-        <div  class="fracaso"ref={left}>
-          <div>Swap me around</div>
-          <div>Swap her around</div>
-          <div>Swap him around</div>
-          <div>Swap them around</div>
+        <div  class="fracaso"ref={fail}>Entrega fallida
+          
         </div>
       </div>
     </div>
